bb03b146aa8edc86834bf8630748a3f0978b42d4
fries <wbtafrier@gmail.com>
2020-07-16 17:03:21 -0400

    Disabling sign movement, button color change

--- Assets/Scripts/SignSwinger.cs
+++ Assets/Scripts/SignSwinger.cs
@@ -2,12 +2,12 @@
 
 public class SignSwinger : MenuElement
 {
-    float maxX = 0f, minX = 0f;
-    float initX = 0f;
-    float currX = 0f;
-    readonly float deltaX = 8f;
-    readonly float maxShift = 5f;
-    bool shiftLeft = true;
+    //float maxX = 0f, minX = 0f;
+    //float initX = 0f;
+    //float currX = 0f;
+    //readonly float deltaX = 8f;
+    //readonly float maxShift = 5f;
+    //bool shiftLeft = true;
 
     // TODO: Remove this.
     bool signDebug = true;
@@ -16,42 +16,42 @@ public class SignSwinger : MenuElement
     {
         base.Start();
 
-        initX = rectTransform.anchoredPosition.x;
-        currX = initX;
-        maxX = initX + maxShift;
-        minX = initX - maxShift;
+        //initX = rectTransform.anchoredPosition.x;
+        //currX = initX;
+        //maxX = initX + maxShift;
+        //minX = initX - maxShift;
     }
 
     // Update is called once per frame
     public override void Update()
     {
         base.Update();
-        float actualDelta = deltaX * Time.deltaTime;
+        //float actualDelta = deltaX * Time.deltaTime;
 
-        if (exiting || stopped)
-        {
-            return;
-        }
+        //if (exiting || stopped)
+        //{
+        //    return;
+        //}
 
-        if (currX <= minX)
-        {
-            shiftLeft = false;
-        }
-        else if (currX >= maxX)
-        {
-            shiftLeft = true;
-        }
+        //if (currX <= minX)
+        //{
+        //    shiftLeft = false;
+        //}
+        //else if (currX >= maxX)
+        //{
+        //    shiftLeft = true;
+        //}
 
-        if (shiftLeft)
-        {
-            currX -= actualDelta;
-        }
-        else
-        {
-            currX += actualDelta;
-        }
+        //if (shiftLeft)
+        //{
+        //    currX -= actualDelta;
+        //}
+        //else
+        //{
+        //    currX += actualDelta;
+        //}
         
-        rectTransform.Translate((shiftLeft ? -actualDelta : actualDelta), 0, 0);
+        //rectTransform.Translate((shiftLeft ? -actualDelta : actualDelta), 0, 0);
 
         if (signDebug)
         {
