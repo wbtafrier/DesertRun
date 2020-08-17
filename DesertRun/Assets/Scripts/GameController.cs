79c107af3b2ffc7023cbba883ff5b5221a830a83
wbtafrier <wbtafrier@gmail.com>
2020-08-17 17:27:08 -0400

    High score counter

--- DesertRun/Assets/Scripts/GameController.cs
+++ DesertRun/Assets/Scripts/GameController.cs
@@ -14,16 +14,15 @@ public class GameController : MonoBehaviour, IStateController
     [SerializeField] GameObject fasterTextBgProp = default;
     [SerializeField] GameObject pauseButtonProp = default;
     [SerializeField] GameObject pauseButtonBgProp = default;
+    [SerializeField] GameObject sessionHighScoreTextBgProp = default;
+    [SerializeField] GameObject sessionHighScoreTextProp = default;
     [SerializeField] GameObject sandProp = default;
     [SerializeField] GameObject playerProp = default;
-    [SerializeField] GameObject cactus1Prop = default;
-    [SerializeField] GameObject cactus2Prop = default;
-    [SerializeField] GameObject rockProp = default;
-    [SerializeField] GameObject snakeProp = default;
 
     static readonly float ROUGH_SCORE_PER_SEC = 50f;
     static readonly float RESTART_DURATION = 1.25f;
     static readonly float TEXT_SPEED_EFFECT_DURATION = 3f;
+    static readonly string SESSION_HIGH_SCORE_TEXT = "YOUR HIGH SCORE: ";
 
     static Camera mainCamera;
     static float roughScore = 0f;
@@ -32,6 +31,8 @@ public class GameController : MonoBehaviour, IStateController
     static bool restarting = false;
     static bool textSpeedEffect = false;
     static float textSpeedEffectTimer = 0f;
+    static int sessionHighScore = 0;
+    static bool newHighScore = false;
 
     static GameObject desertGeneratorObj;
     static GameObject scoreTextObj;
@@ -40,22 +41,23 @@ public class GameController : MonoBehaviour, IStateController
     static GameObject fasterBgTextObj;
     static GameObject pauseButtonObj;
     static GameObject pauseButtonBgObj;
+    static GameObject sessionHighScoreTextBgObj;
+    static GameObject sessionHighScoreTextObj;
     static GameObject sand;
     static GameObject meloReloObj;
-    static GameObject cactus1;
-    static GameObject cactus2;
-    static GameObject rock;
-    static GameObject snake;
 
     static DesertGenerator desertGenerator;
     static TextMeshProUGUI scoreText;
     static TextMeshProUGUI scoreBgText;
     static TextMeshProUGUI fasterText;
     static TextMeshProUGUI fasterBgText;
+    static TextMeshProUGUI sessionHighScoreTextBg;
+    static TextMeshProUGUI sessionHighScoreText;
     static Button pauseButton;
     static MeloRelo meloReloComp;
 
     static Color scoreTextDefaultColor;
+    static Color sessionHighScoreTextDefaultColor;
 
     // Start is called before the first frame update
     void Start()
@@ -66,24 +68,28 @@ public class GameController : MonoBehaviour, IStateController
         scoreBgTextObj = scoreBgProp;
         fasterTextObj = fasterTextProp;
         fasterBgTextObj = fasterTextBgProp;
+        sessionHighScoreTextBgObj = sessionHighScoreTextBgProp;
+        sessionHighScoreTextObj = sessionHighScoreTextProp;
         pauseButtonObj = pauseButtonProp;
         pauseButtonBgObj = pauseButtonBgProp;
         sand = sandProp;
         meloReloObj = playerProp;
-        cactus1 = cactus1Prop;
-        cactus2 = cactus2Prop;
-        rock = rockProp;
-        snake = snakeProp;
 
         desertGenerator = desertGeneratorObj.GetComponent<DesertGenerator>();
         scoreText = scoreTextObj.GetComponent<TextMeshProUGUI>();
         scoreBgText = scoreBgTextObj.GetComponent<TextMeshProUGUI>();
         fasterText = fasterTextObj.GetComponent<TextMeshProUGUI>();
         fasterBgText = fasterBgTextObj.GetComponent<TextMeshProUGUI>();
+        sessionHighScoreTextBg = sessionHighScoreTextBgObj.GetComponent<TextMeshProUGUI>();
+        sessionHighScoreText = sessionHighScoreTextObj.GetComponent<TextMeshProUGUI>();
         pauseButton = pauseButtonObj.GetComponent<Button>();
         meloReloComp = meloReloObj.GetComponent<MeloRelo>();
 
+        string sessionHighScoreStr = sessionHighScore.ToString();
         scoreTextDefaultColor = scoreText.color;
+        sessionHighScoreTextDefaultColor = sessionHighScoreText.color;
+        sessionHighScoreText.text = SESSION_HIGH_SCORE_TEXT + sessionHighScoreStr;
+        sessionHighScoreTextBg.text = SESSION_HIGH_SCORE_TEXT + sessionHighScoreStr;
     }
 
     public void OnStateEnable()
@@ -92,11 +98,9 @@ public class GameController : MonoBehaviour, IStateController
         desertGeneratorObj.SetActive(true);
         scoreTextObj.SetActive(true);
         scoreBgTextObj.SetActive(true);
+        sessionHighScoreTextObj.SetActive(true);
+        sessionHighScoreTextBgObj.SetActive(true);
         meloReloObj.SetActive(true);
-        //cactus1.SetActive(true);
-        //cactus2.SetActive(true);
-        //rock.SetActive(true);
-        //snake.SetActive(true);
         desertGenerator.OnGenEnable();
         Restart();
     }
@@ -105,17 +109,23 @@ public class GameController : MonoBehaviour, IStateController
     {
         ResetScoreTextColor();
 
+        if (sessionHighScoreText.color.Equals(Color.red))
+        {
+            sessionHighScoreText.color = sessionHighScoreTextDefaultColor;
+            sessionHighScoreText.fontStyle = FontStyles.Normal;
+            sessionHighScoreTextBg.fontStyle = FontStyles.Normal;
+        }
+        newHighScore = true;
+
         desertGeneratorObj.SetActive(false);
         scoreTextObj.SetActive(false);
         scoreBgTextObj.SetActive(false);
+        sessionHighScoreTextObj.SetActive(false);
+        sessionHighScoreTextBgObj.SetActive(false);
         pauseButtonObj.SetActive(false);
         pauseButtonBgObj.SetActive(false);
         meloReloObj.SetActive(false);
         desertGenerator.OnGenDisable();
-        //cactus1.SetActive(false);
-        //cactus2.SetActive(false);
-        //rock.SetActive(false);
-        //snake.SetActive(false);
     }
 
     public void Pause()
@@ -146,6 +156,14 @@ public class GameController : MonoBehaviour, IStateController
         scoreBgText.text = scoreStr;
         scoreText.text = scoreStr;
         ResetScoreTextColor();
+
+        if (sessionHighScoreText.color.Equals(Color.red))
+        {
+            sessionHighScoreText.color = sessionHighScoreTextDefaultColor;
+            sessionHighScoreText.fontStyle = FontStyles.Normal;
+            sessionHighScoreTextBg.fontStyle = FontStyles.Normal;
+        }
+
         pauseButtonBgObj.SetActive(true);
         pauseButtonObj.SetActive(true);
         pauseButton.interactable = false;
@@ -162,6 +180,24 @@ public class GameController : MonoBehaviour, IStateController
             }
 
             string scoreStr = score.ToString();
+
+            if (score > sessionHighScore)
+            {
+                if (newHighScore)
+                {
+                    if (!sessionHighScoreText.color.Equals(Color.red))
+                    {
+                        sessionHighScoreText.color = Color.red;
+                        sessionHighScoreText.fontStyle = FontStyles.Italic;
+                        sessionHighScoreTextBg.fontStyle = FontStyles.Italic;
+                    }
+                }
+
+                sessionHighScore = score;
+                sessionHighScoreText.text = SESSION_HIGH_SCORE_TEXT + scoreStr;
+                sessionHighScoreTextBg.text = SESSION_HIGH_SCORE_TEXT + scoreStr;
+            }
+
             roughScore += ROUGH_SCORE_PER_SEC * Time.deltaTime;
             score = (int)roughScore;
             scoreBgText.text = scoreStr;
@@ -234,6 +270,7 @@ public class GameController : MonoBehaviour, IStateController
     {
         if (!GameStateMachine.IsGameOver())
         {
+            newHighScore = true;
             pauseButtonBgObj.SetActive(false);
             pauseButtonObj.SetActive(false);
             meloReloComp.Die();
