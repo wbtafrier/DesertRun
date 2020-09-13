1d3618f0d481973018c629a8c3cf3896d6b75dc0
wbtafrier <wbtafrier@gmail.com>
2020-09-13 09:26:46 -0400

    Skull balloons and new coins

---
+++ DesertRun/Assets/Scripts/SkullExplosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullExplosion : MonoBehaviour
+{
+    private void OnParticleSystemStopped()
+    {
+        SkullBalloon sb = transform.GetComponentInParent<SkullBalloon>();
+        sb.ReturnToInitialPosition();
+        sb.Deactivate();
+        transform.GetComponentInParent<SpriteRenderer>().enabled = true;
+    }
+}
